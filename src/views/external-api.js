2a50f3e9065c2340cca429b575f234ed71942815
FischerEnrique <e.fischer@8erlin.de>
2022-11-16 18:25:54 +0100

    https

--- src/views/external-api.js
+++ src/views/external-api.js
@@ -3,7 +3,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 
 const ExternalApi = () => {
   const [message, setMessage] = useState('');
-  const serverUrl = 'http://wp.artelocal.eu/api/menu/items';
+  const serverUrl = 'https://wp.artelocal.eu/api/menu/items';
 
   const { getAccessTokenSilently } = useAuth0();
 
